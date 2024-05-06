6c3a844d95709b469a88e0b7ce908e38190f3699
Maria Usina <maryusina@Marias-Air.attlocal.net>
2024-05-06 18:28:57 -0500

    changes made for May 9th class

--- arrays/exercises/part-five-arrays.js
+++ arrays/exercises/part-five-arrays.js
@@ -2,10 +2,18 @@ let str = 'In space, no one can hear you code.';
 let arr = ['B', 'n', 'n', 5];
 
 //1) Use the split method on the string to identify the purpose of the parameter inside the ().
-
+console.log(str.split()); // convert string into array
+console.log(str.split('e')); //convert string into array and remove all "e"
+console.log(str.split(' '));
+console.log(str.split(''));
 //2) Use the join method on the array to identify the purpose of the parameter inside the ().
-
+console.log(arr.join());  // B,n,n,5 convert array into a string
+console.log(arr.join(''));  // Bnn5 convert array into a string and remove all spaces
+console.log(arr.join(' '));  // B n n 5
+console.log(arr.join(','));  // B,n,n,5
+console.log(arr.join(', '));  // B, n, n, 5
 //3) Do split or join change the original string/array?
 
 //4) We can take a comma-separated string and convert it into a modifiable array. Try it! Alphabetize the cargoHold string, and then combine the contents into a new string.
 let cargoHold = "water,space suits,food,plasma sword,batteries";
+console.log(cargoHold.split(',').sort().join(','));
--- arrays/exercises/part-two-arrays.js
+++ arrays/exercises/part-two-arrays.js
@@ -1,11 +1,20 @@
 let cargoHold = ['oxygen tanks', 'space suits', 'parrot', 'instruction manual', 'meal packs', 'slinky', 'security blanket'];
-
+console.log(cargoHold);
 //1) Use bracket notation to replace ‘slinky’ with ‘space tether’. Print the array to confirm the change.
-
+cargoHold[5]='space tether';
+console.log(cargoHold);
 //2) Remove the last item from the array with pop. Print the element removed and the updated array.
-
+console.log(cargoHold[6]); //security blanket
+cargoHold.pop(6);
+console.log(cargoHold);
 //3) Remove the first item from the array with shift. Print the element removed and the updated array.
-
+console.log(cargoHold[0]); //oxygen tanks
+cargoHold.shift(0);
+console.log(cargoHold);
 //4) Unlike pop and shift, push and unshift require arguments inside the (). Add the items 1138 and ‘20 meters’ to the the array - the number at the start and the string at the end. Print the updated array to confirm the changes.
-
+cargoHold.unshift(1138); //add 1138 in the begging of the array
+console.log(cargoHold);
+cargoHold.push('20 meters'); //add '20 meters' in the end of array
+console.log(cargoHold);
 //5) Use a template literal to print the final array and its length.
+console.log(`The final array ${cargoHold} has a length of ${cargoHold.length}.`);
