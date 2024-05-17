0d3fd452a0562f006756998a8737f92cbe4171be
Maria Usina <maryusina@Marias-MacBook-Air.local>
2024-05-16 20:42:06 -0500

    Studio done MAY 16

--- loops/chapter-examples/Loop-Variable.js
+++ loops/chapter-examples/Loop-Variable.js
@@ -1,5 +1,17 @@
 // Experiment with this loop by modifying each of the following: the variable initialization, the boolean condition, and the update expression.
 
-for (let i = 0; i < 51; i++) {
-   console.log(i);
- }
+// for (let i = 0; i < 51; i++) {
+//    console.log(i);
+//  }
+
+let phrase = "LaunchCode's LC101";
+
+for (let i = 0; i < phrase.length - 1; i = i + 3) {
+   console.log(phrase[i]);
+}
+
+let phrase1 = "Chili Cook-off";
+
+for (let i = 0; i < phrase1.length - 1; i = i + 3) {
+   console.log(phrase1[i]);
+}
--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,7 @@
 // create an array variable containing the names
+let names = ["Maria", "Mike", "Svetlana", "Sergey"];
 
 // write a for loop that prints each name on a different line
+for (let i = 0; i < names.length; i++) {
+   console.log(names[i]);
+}
